256755f5602673ccce40fc57d496674f14db8ff3
Bartosz Piotr Cłapiński <bartosz.clapinski@gmail.com>
2022-08-10 09:20:02 +0200

    Added diagonal difference solution

--- Program.cs
+++ Program.cs
@@ -88,6 +88,18 @@
         return (long) result;
     }
 
+    public static int diagonalDifference(List<List<int>> arr)
+    {
+        int sum = 0;
+        
+        for (int i = 0; i < arr.Count; i++)
+        {            
+            sum += arr[i][i] - arr[i][arr[i].Count - i - 1];            
+        }       
+
+        return Math.Abs(sum);
+    }
+
     public static void Main(string[] args)
     {
         List<int> plusMinusArr = new List<int> { 1, 1, 0, -1, -1 };
@@ -115,5 +127,16 @@
 
         long n = 9;
         flippingBits(n);
+
+        List<List<int>> diagonalDifferenceArr = new List<List<int>>
+            {
+                new List<int> { 1, 2, 3 },
+                new List<int> { 4, 5, 6 },
+                new List<int> { 9, 8, 9 }
+            };
+        Console.WriteLine(diagonalDifference(diagonalDifferenceArr));
+
+        
+
     }
 }
