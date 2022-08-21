d372d5d8fffaf4f1524a1fa8426529b1dc0d4afd
Bartosz Piotr Cłapiński <bartosz.clapinski@gmail.com>
2022-08-21 19:12:49 +0200

    Added max min solution

---
+++ HackerRank/MaxMin/MaxMin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTestSolutions.HackerRank.MaxMin
+{
+    class MaxMin
+    {
+        public static int maxMin(int k, List<int> arr)
+        {
+            int result = Int32.MaxValue, temp = 0;
+            arr.Sort();            
+
+            for (int i = 0; i < arr.Count - k + 1; i++)
+            {
+                temp = Math.Max(arr[i], arr[i + k - 1]) - Math.Min(arr[i], arr[i + k - 1]);
+                result = Math.Min(temp, result);
+            }
+            return result;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,8 +4,8 @@ class Program
 {
     public static void Main(string[] args)
     {
-        string s = "1X7T4VrCs23k4vv08D6yQ3S19G4rVP188M9ahuxB6j1tMGZs1m10ey7eUj62WV2exLT4C83zl7Q80M";
-        int k = 27;
-        Console.WriteLine(ConsoleTestSolutions.HackerRank.CaesarCipher.CaesarCipher.caesarCipher(s, k));
+        List<int> list = new List<int>() { 7, 3, 100, 200, 300, 350, 400, 401, 402 };
+        int k = 3;
+        Console.WriteLine(ConsoleTestSolutions.HackerRank.MaxMin.MaxMin.maxMin(k, list));
     }
 }
