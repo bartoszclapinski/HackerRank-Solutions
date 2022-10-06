52c8e8d301bd60bd1c50a7266d0e4b60529ace17
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2022-10-06 20:17:49 +0200

    Added counter game solution in c#

--- Program.cs
+++ Program.cs
@@ -4,8 +4,6 @@ class Program
 {
     public static void Main(string[] args)
     {
-        string n = "0000000009875";
-        int k = 4;
-        ConsoleTestSolutions.HackerRank.RecursiveDigitSum.RecursiveDigitSum.superDigit(n, k);
+        
     }
 }
